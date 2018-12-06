dfb867cb135092da773a0c95d4e2989eb9d4b47c
Tommy Hui <aesops0124@gmail.com>
2018-12-06 14:15:54 +0800

    Add About, users, news

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
-
+import {Route, Link, Switch} from 'react-router-dom'
+import AboutPage from './components/AboutPage'
+import NotFoundPage from './components/NotFoundPage'
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <div>
-            <ul>
+        <nav>
+          <ul>
               <li>
-                <Link to="/about">about</Link>
+                <Link to="/about">About</Link>
               </li>
-            </ul>
-            <Route path="/about" component={() => <h2>About</h2>}></Route>
-          </div>
-        </Router>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/news">News</Link>
+              </li>
+
+          </ul>
+        </nav>
+        <hr/>
+        <Switch>
+          <Route path="/" exact component={() => <h1>home page</h1>}></Route>
+          <Route path="/about" component={AboutPage}></Route>
+          <Route path="/users" component={() => <h1>Users page</h1>}></Route>
+          <Route path="/news" component={() => <h1>News page</h1>}></Route>
+          <Route component={NotFoundPage}></Route>
+        </Switch>
       </div>
-    );
+    ) ;       
   }
 }
 
